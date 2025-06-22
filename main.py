eec3d8326e60b52354454acb285b0dacc5e463c8
aoyagi <thisisaoyagi@gmail.com>
2025-06-23 00:37:18 +0900

    웹서버가 열리지 않도록 수정

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from flask import Flask
 import requests
-import schedule
-import time
-import threading
 
 # LINE API 정보
 LINE_ACCESS_TOKEN = 'WLHqXsJMK8wepoyn9Eg8TVMGy+P/3zpB3eLuF1LltSnfUYaTG+dEDX38BWGWR/UVwokEm9qJg00/FItP7A75nlD/XUG88wKswEWZAHJmd7fqGHgUK5fEhV9ILH6Wjli5qDS/Tv//jWOlKp9c5TjefQdB04t89/1O/w1cDnyilFU='
@@ -18,13 +15,6 @@ app = Flask(__name__)
 def get_weather():
     url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric&lang=kr"
     res = requests.get(url).json()
-
-    
-    # weather = res["weather"][1]["main"]
-    # temperature = res["main"]["temp"]
-    # f"오늘 {CITY}의 날씨: {weather}, 기온: {temperature}°C"
-
-
     weatherInfo = res["weather"]
     print(weatherInfo)
 
@@ -51,23 +41,9 @@ def daily_task():
     message = get_weather()
     send_line_message(message)
 
-def schedule_runner():
-    # 매일 오전 7시 30분에 실행
-    schedule.every().day.at("07:30").do(daily_task)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
-
 @app.route('/')
 def index():
     return 'LINE 날씨 알림 서버 동작 중!'
 
 if __name__ == '__main__':
     daily_task()
-
-    # 스케줄러는 백그라운드 쓰레드로 실행
-    threading.Thread(target=schedule_runner, daemon=True).start()
-
-    # Flask 서버 시작
-    app.run(host='0.0.0.0', port=5000)
